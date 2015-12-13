a2c0e6f263d41f4877489ac33aaa6fc87769f419
nelsonjma <nelsonjma@hotmail.com>
2015-12-13 23:57:38 +0000

    uart hub
    testes quase completos.

--- modulos/uart_hub/code/master/master_real_serial/master_real_serial.ino
+++ modulos/uart_hub/code/master/master_real_serial/master_real_serial.ino
@@ -1,3 +1,12 @@
+const String ARDUINO_NAME = "MASTER";
+
+struct MGS_PARTS {
+  String origin;
+  String destination;
+  String operation;
+  String data;
+};
+
 void setup() {
 	Serial.begin(9600);
   while (!Serial) {
@@ -6,10 +15,121 @@ void setup() {
 }
 
 void loop() {
-	Serial.println("master:slave1:hello world");
-	delay(1000);
-	Serial.println("master:slave2:hello world");
-	delay(1000);
-	Serial.println("master:slave3:hello world");
-	delay(1000);
+	hello("PUMP");
+	delay(10000);
+  hello("GPS");
+  delay(10000);
 }
+
+/**************************************************
+ * Wait for something in the serial channel
+ **************************************************/
+struct MGS_PARTS listeningSerialChannel() {
+  String content = ""; // complete message
+  char character; // one character
+  
+  while(true) {
+
+    if (Serial.available() > 0) {
+      character = Serial.read();
+      content.concat(character);
+    }
+
+    if (character == 13) {
+      return buildMessageParts(content);
+    }
+  }
+}
+
+/**************************************************
+ * Identification
+ **************************************************/
+boolean isThisMe(String name) {
+  return (name == ARDUINO_NAME) ? true : false;
+}
+
+/**************************************************
+ * Parse raw message and build each message part 
+ **************************************************/
+struct MGS_PARTS buildMessageParts(String message) {
+  // Ori:Dest:Opr:Data
+  int endOri = message.indexOf(':'); // Ori:
+  int endDest = message.indexOf(':', endOri + 1); // Dest:
+  int endOpr = message.indexOf(':', endDest + 1); // Opr:
+
+  struct MGS_PARTS messageParts;
+
+  if (endOri != -1 && endDest != -1) {
+    String origin = message.substring(0, endOri);
+    origin.trim();
+
+    String destination = message.substring(endOri + 1, endDest);
+    destination.trim();
+
+    String operation = (endOpr != -1 ? message.substring(endDest + 1, endOpr) : message.substring(endDest + 1, message.length() - 1));
+    operation.trim();
+
+    String data = (endOpr != -1 ? message.substring(endOpr + 1) : "null");
+    data.trim();
+    
+    messageParts = { origin, destination, operation, data };
+  } else {
+    messageParts = { "null", "null", "null", "null" };
+  }
+
+  return messageParts;
+}
+
+/**************************************************
+ * Rebuild response message
+ **************************************************/
+String buildResponseMessage(struct MGS_PARTS &msgParts) {
+  String message = "";
+
+  message.concat(msgParts.destination);
+  message.concat(":");
+  message.concat(msgParts.origin);
+  message.concat(":");
+  message.concat(msgParts.operation);
+  message.concat(":");
+  message.concat(msgParts.data);
+
+  return message;
+}
+
+/**************************************************
+ * Operations
+ **************************************************/
+
+ void hello(String destination) {
+
+    String sendMessage = "MASTER:";
+    sendMessage.concat(destination);
+    sendMessage.concat(":AYT");
+
+    Serial.println(sendMessage);
+
+    while(true) {
+      struct MGS_PARTS msgParts = listeningSerialChannel();
+  
+      String orig = msgParts.origin;
+      String dest = msgParts.destination;
+      String opr = msgParts.operation;
+      String data = msgParts.data;
+  
+      if (dest == ARDUINO_NAME && opr == "AYT") {
+        return;
+      }
+      else {
+        Serial.println(".............");
+        Serial.println("#" + orig + "#");
+        Serial.println("#" + dest + "#");
+        Serial.println("#" + opr + "#");
+        Serial.println("#" + data + "#");
+        Serial.println(".............");
+      }
+    }
+
+    return;
+ }
+
--- modulos/uart_hub/code/slave/slave_real_serial/slave_real_serial.ino
+++ modulos/uart_hub/code/slave/slave_real_serial/slave_real_serial.ino
@@ -1,4 +1,11 @@
-const String ArduinoName = "slave1";
+const String ARDUINO_NAME = "GPS";
+
+struct MGS_PARTS {
+  String origin;
+  String destination;
+  String operation;
+  String data;
+};
 
 void setup() {
   Serial.begin(9600);
@@ -8,18 +15,21 @@ void setup() {
 }
 
 void loop() {
-  String message = listeningSerialChannel();
+  //String message = listeningSerialChannel();
 
-  if (isThisMe(message)) {
-    Serial.println("yes");
-  } else {
-    Serial.println("no");
+  struct MGS_PARTS msgParts = listeningSerialChannel();
+
+  if (isThisMe(msgParts.destination)) {
+    runOperation(msgParts);
+    Serial.println(buildResponseMessage(msgParts)); 
   }
-
-  Serial.println(getOperation(message));
 }
 
-String listeningSerialChannel() {
+
+/**************************************************
+ * Wait for something in the serial channel
+ **************************************************/
+struct MGS_PARTS listeningSerialChannel() {
   String content = ""; // complete message
   char character; // one character
   
@@ -31,33 +41,120 @@ String listeningSerialChannel() {
     }
 
     if (character == 13) {
-      return content;
+      return buildMessageParts(content);
     }
   }
 }
 
-boolean isThisMe(String message) {
-  // master:slave:operation
-  int start = message.indexOf(':'); // master:
-  int end = message.indexOf(':', start + 1); // slave:
-
-  if (start != -1 && end != -1) {
-    if (message.substring(start + 1, end) == ArduinoName) {
-      return true;
-    }
-  }
-  
-  return false;
+/**************************************************
+ * Identification
+ **************************************************/
+boolean isThisMe(String name) {
+  return (name == ARDUINO_NAME) ? true : false;
 }
 
-String getOperation(String message) {
-  // master:slave:operation
-  int start = message.indexOf(':'); // master:
-  int end = message.indexOf(':', start + 1); // slave:
+/**************************************************
+ * Parse raw message and build each message part 
+ **************************************************/
+struct MGS_PARTS buildMessageParts(String message) {
+  // Ori:Dest:Opr:Data
+  int endOri = message.indexOf(':'); // Ori:
+  int endDest = message.indexOf(':', endOri + 1); // Dest:
+  int endOpr = message.indexOf(':', endDest + 1); // Opr:
 
-  if (start != -1 && end != -1) {
-    return message.substring(end + 1);  
+  struct MGS_PARTS messageParts;
+
+  if (endOri != -1 && endDest != -1) {
+    String origin = message.substring(0, endOri);
+    origin.trim();
+
+    String destination = message.substring(endOri + 1, endDest);
+    destination.trim();
+
+    String operation = (endOpr != -1 ? message.substring(endDest + 1, endOpr) : message.substring(endDest + 1, message.length() - 1));
+    operation.trim();
+
+    String data = (endOpr != -1 ? message.substring(endOpr + 1) : "null");
+    data.trim();
+    
+    messageParts = { origin, destination, operation, data };
+  } else {
+    messageParts = { "null", "null", "null", "null" };
   }
 
-  return "null";
+  return messageParts;
 }
+
+/**************************************************
+ * Rebuild response message
+ **************************************************/
+String buildResponseMessage(struct MGS_PARTS &msgParts) {
+  String message = "";
+
+  message.concat(msgParts.destination);
+  message.concat(":");
+  message.concat(msgParts.origin);
+  message.concat(":");
+  message.concat(msgParts.operation);
+  message.concat(":");
+  message.concat(msgParts.data);
+
+  return message;
+}
+
+/**************************************************
+ * Operations
+ **************************************************/
+
+/* hello response */
+String helloResponse() {
+  return "OK";
+}
+
+String readGps() {
+  return "41 24.2028, 2 10.4418";
+}
+
+String readPumpStatus() {
+  return "1";
+}
+
+String readWaterTank() {
+  return "3";
+}
+
+String read3gOnlineStatus() {
+  return "1";
+}
+
+/**************************************************
+ * Operations Manager
+ **************************************************/
+void runOperation(struct MGS_PARTS &msgParts) {
+  String opr = msgParts.operation;
+  String dest = msgParts.destination;
+
+  if (opr == "AYT") {
+    
+    msgParts.data = helloResponse();
+        
+  } else if (opr == "STATUS" && dest == "PUMP") {
+    
+    msgParts.data = readPumpStatus();
+    
+  } else if (opr == "LEVEL" && dest == "WTANK") {
+    
+    msgParts.data = readWaterTank();
+    
+  } else if (opr == "WAI" && dest == "GPS") {
+    
+    msgParts.data = readGps();
+    
+  } else if (opr == "ISONLINE" && dest == "3G") {
+    
+    msgParts.data = read3gOnlineStatus();
+    
+  }
+}
+
+
