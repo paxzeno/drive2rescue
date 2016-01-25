bfd0f9da004b99ad4eb5918139ab5282c53267b5
nelsonjma <nelsonjma@hotmail.com>
2016-01-25 01:45:30 +0000

    tunning 3g code
    melhorar o codigo do modulo 3g

--- modulos/3g - sim808/demos/get_operation.ino
+++ modulos/3g - sim808/demos/get_operation.ino
@@ -1,68 +1,84 @@
 #include <SoftwareSerial.h>
 
-//#define _SS_MAX_RX_BUFF 256 // RX buffer size //BEFORE WAS 64
-
 SoftwareSerial softSerialB(10, 11); // RX, TX
 
+char dataContainer[1000];
+int dataContainerLength = 0;
+int dataContainerPosition = 0;
+int maxContainerCaracters = 1000;
+
 void setup() {
-  // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+    ;
   }
 
   softSerialB.begin(4800);
 }
 
 void loop() {
+
+  char ok[3] =  {'O', 'K', '\r'};
+  int okLength = sizeof(ok);
+  char httpAction[11] = {'H', 'T', 'T', 'P', 'A', 'C', 'T', 'I', 'O', 'N', ':'};
+  int httpActionLength = sizeof(httpAction);
   
-  runCommand("AT+CREG?", "OK\r");  
-  runCommand("AT+CSQ", "OK\r");
+  runCommand("AT+CREG?", ok, okLength);
+  runCommand("AT+CSQ", ok, okLength);
 
-  runCommand("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"", "OK\r");
-  runCommand("AT+SAPBR=3,1,\"APN\",\"net2.vodafone.pt\"", "OK\r");
-  runCommand("AT+SAPBR=3,1,\"USER\",\"\"", "OK\r");
-  runCommand("AT+SAPBR=3,1,\"PWD\",\"\"", "OK\r");
+  runCommand("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"", ok, okLength);
+  runCommand("AT+SAPBR=3,1,\"APN\",\"net2.vodafone.pt\"", ok, okLength);
+  runCommand("AT+SAPBR=3,1,\"USER\",\"\"", ok, okLength);
+  runCommand("AT+SAPBR=3,1,\"PWD\",\"\"", ok, okLength);
 
-  runCommand("AT+SAPBR=0,1", "OK\r");
-  runCommand("AT+SAPBR=1,1", "OK\r");
-  runCommand("AT+SAPBR=2,1", "OK\r");
+  runCommand("AT+SAPBR=0,1", ok, okLength);
+  runCommand("AT+SAPBR=1,1", ok, okLength);
+  runCommand("AT+SAPBR=2,1", ok, okLength);
 
-  runCommand("AT+HTTPTERM", "OK\r");
-  runCommand("AT+HTTPINIT", "OK\r");
-  runCommand("AT+HTTPPARA=\"CID\",1", "OK\r");
-  runCommand("AT+HTTPPARA=\"URL\",\"http://www.m2msupport.net/m2msupport/test.php\"", "OK\r");
-  //runCommand("AT+HTTPPARA=\"URL\",\"http://www.xxx.php\"", "OK\r");
-  runCommand("AT+HTTPACTION=0", "HTTPACTION:");
-  runCommand("AT+HTTPREAD", "OK\r");
-  runCommand("AT+HTTPTERM", "OK\r");
-  runCommand("AT+SAPBR=0,1", "OK\r");
+  runCommand("AT+HTTPTERM", ok, okLength);
+  runCommand("AT+HTTPINIT", ok, okLength);
+  runCommand("AT+HTTPPARA=\"CID\",1", ok, okLength);
+  runCommand("AT+HTTPPARA=\"URL\",\"http://www.m2msupport.net/m2msupport/test.php\"", ok, okLength);
+  runCommand("AT+HTTPACTION=0", httpAction, httpActionLength);
+  runCommand("AT+HTTPREAD", ok, okLength);
+  runCommand("AT+HTTPTERM", ok, okLength);
+  runCommand("AT+SAPBR=0,1", ok, okLength);
 
-  delay(20000);
+  delay(60000);
 
 }
 
-void runCommand(String command, String endingMessage) {
-  
+void runCommand(String command, char endMsg[], int endMsgLength) {
   sendToGsm(command);
 
-  String returnData = waitForGsmResponse(endingMessage);
-  Serial.println(returnData);
+  waitForGsmResponse(endMsg, endMsgLength);
+
+  // print data
+  resetContainerPosition();
+  while(!currentPositionEqualsLength()){
+    Serial.print(getCurrentChar());
+  }
+  restartContainer();
 }
 
 void sendToGsm(String command) {
-  if (command == "") { return; }
+  if (command == "")
+    return; 
   softSerialB.println(command);
 }
 
-String waitForGsmResponse(String endingMessage) {
-  String content = ""; // complete message
-  char character = 0; // one character
+void waitForGsmResponse(char endMsg[], int endMsgLength) {
+  char character = 0; // empty caracter
 
-  int endingMessageLength = endingMessage.length();
-  char firstChar = endingMessage.charAt(0);
-  String comparator = "";
-  boolean comparatorStart = false;
+  // end message
+  int possibleEndMsgPosition = 0;
+  boolean isPossibleEndMsg = false;
+
+  // error message
+  char errorMsg[6] =  {'E', 'R', 'R', 'O', 'R', '\r'};
+  int errorMsgLength = sizeof(errorMsg);
+  int possibleErrorMsgPosition = 0;
+  boolean isPossibleErrorMsg = false;
 
   softSerialB.flush();
 
@@ -72,42 +88,86 @@ String waitForGsmResponse(String endingMessage) {
       
       if (character != (char)-1){
         //Serial.write(character);
-        content += character;
+        addChar(character);
       }
 
-      if (gsmResponseSelector(firstChar, comparatorStart, character, comparator, endingMessageLength, endingMessage)) {
-        return content;
+      if (isEndMessage(isPossibleEndMsg, possibleEndMsgPosition, endMsg, endMsgLength, character)) {
+        return;
+      } else if (isEndMessage(isPossibleErrorMsg, possibleErrorMsgPosition, errorMsg, errorMsgLength, character)) {
+        return;
       }
-    }
-    
-    if (content.length() > 0 && content.length() < 100 && content.indexOf("ERROR\r") != -1) {
-      return content;
+      
     }
   }
 }
 
+boolean isEndMessage(boolean &isPossibleMsg, int &possibleMsgPosition, char msg[], int &msgLength, char character) {
 
-boolean gsmResponseSelector(char &firstChar, boolean &comparatorStart, char character, String &comparator, int &endingMessageLength, String &endingMessage) {
-
-  int comparatorLength = comparator.length();
+  if (!isPossibleMsg && character == msg[0]) {
+    isPossibleMsg = true;
+    possibleMsgPosition = 1;
+  } else if (isPossibleMsg) {
+    if (possibleMsgPosition >= msgLength) {
+      possibleMsgPosition = 0;
+      isPossibleMsg = false;
+      return true;
+    } else if (character == msg[possibleMsgPosition] && possibleMsgPosition < msgLength) {
+      possibleMsgPosition++;
+    } else if (character != msg[possibleMsgPosition]) {
+      possibleMsgPosition = 0;
+      isPossibleMsg = false;
+    }
+  }
   
-  if (firstChar == character) {
-        comparatorStart = true;
-  }
-
-  if (comparatorStart && comparatorLength != endingMessageLength) {
-    comparator += character;  
-  } else if (comparatorStart && comparatorLength == endingMessageLength && comparator == endingMessage) {
-    return true;
-  } else if (comparatorStart && comparatorLength == endingMessageLength && comparator != endingMessage) {
-    comparator = "";
-    comparatorStart = false;
-  }
-
   return false;
 }
 
 
 
+/*************************************************
+ * Container Management
+ *************************************************/
 
+// add new character
+void addChar(char value) {
+  if (dataContainerLength >= maxContainerCaracters) 
+    return;
+    
+  dataContainer[dataContainerLength] = value;
+  dataContainerLength++;
+}
+
+// get next character
+char getCurrentChar() {
+  if (dataContainerPosition >= dataContainerLength)
+    return -1;
+
+  dataContainerPosition++;
+  return dataContainer[dataContainerPosition-1];
+}
+
+// is container full ?
+boolean containerFull() {
+  return (dataContainerLength < maxContainerCaracters) ? true : false;
+}
+
+// is current posistion equals to length
+boolean currentPositionEqualsLength() {
+  return (dataContainerPosition < dataContainerLength) ? false : true;
+}
+
+// reset container parse position
+void resetContainerPosition() {
+  dataContainerPosition = 0;
+}
+
+// restart container so that new data can be added
+void restartContainer() {
+  dataContainerLength = 0;
+  dataContainerPosition = 0;
+}
+
+/*************************************************
+ *************************************************
+ *************************************************/ 
 
