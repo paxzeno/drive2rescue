cdd26645823c8868676035727fa037bbf0859135
paxzeno <paxzeno@gmail.com>
2016-02-21 17:46:05 +0000

    [queues] tweak the sender usability

--- queue-client/sender.py
+++ queue-client/sender.py
@@ -1,16 +1,20 @@
 import pika
 import config
+import message_utils
 
+carConfig = config.config_section_map('MyCar')
 queueConfig = config.config_section_map('QueueConnection')
 url = queueConfig['server_url']
+car_id = carConfig['vehicle_id']
 
 
 def send(message):
+    byte_message = message_utils.prepare_message(message)
     connection = pika.BlockingConnection(pika.ConnectionParameters(url))
     channel = connection.channel()
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue=car_id)
     channel.basic_publish(exchange='',
                           routing_key='hello',
-                          body=message)
-    print(" [x] Sent 'Hello World!'")
+                          body=byte_message)
+    print " [x] Sent to queue[%s] message: %s" % (car_id, message)
     connection.close()
